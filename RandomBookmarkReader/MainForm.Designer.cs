0020dec6af5df253d1a9d90137c583816e776824
Biscuits <marc.b@live.co.uk>
2020-11-05 13:17:14 +0000

    Changes to the visual styles of the main form

--- RandomBookmarkReader/MainForm.Designer.cs
+++ RandomBookmarkReader/MainForm.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.RefreshFoldersButton = new System.Windows.Forms.Button();
             this.FoldersTreeView = new System.Windows.Forms.TreeView();
             this.IncludesSubFoldersCheckbox = new System.Windows.Forms.CheckBox();
@@ -45,36 +49,53 @@
             // 
             // RefreshFoldersButton
             // 
-            this.RefreshFoldersButton.Location = new System.Drawing.Point(1, 3);
+            this.RefreshFoldersButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RefreshFoldersButton.BackColor = System.Drawing.Color.Black;
+            this.RefreshFoldersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RefreshFoldersButton.ForeColor = System.Drawing.Color.White;
+            this.RefreshFoldersButton.Location = new System.Drawing.Point(1, -1);
             this.RefreshFoldersButton.Name = "RefreshFoldersButton";
-            this.RefreshFoldersButton.Size = new System.Drawing.Size(543, 45);
+            this.RefreshFoldersButton.Size = new System.Drawing.Size(733, 45);
             this.RefreshFoldersButton.TabIndex = 0;
             this.RefreshFoldersButton.Text = "Refresh folders";
-            this.RefreshFoldersButton.UseVisualStyleBackColor = true;
+            this.RefreshFoldersButton.UseVisualStyleBackColor = false;
             this.RefreshFoldersButton.Click += new System.EventHandler(this.RefreshFoldersButton_Click);
             // 
             // FoldersTreeView
             // 
-            this.FoldersTreeView.Location = new System.Drawing.Point(1, 50);
+            this.FoldersTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FoldersTreeView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.FoldersTreeView.Enabled = false;
+            this.FoldersTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FoldersTreeView.Location = new System.Drawing.Point(1, 43);
             this.FoldersTreeView.Name = "FoldersTreeView";
-            this.FoldersTreeView.Size = new System.Drawing.Size(543, 267);
+            this.FoldersTreeView.Size = new System.Drawing.Size(733, 267);
             this.FoldersTreeView.TabIndex = 1;
             // 
             // IncludesSubFoldersCheckbox
             // 
+            this.IncludesSubFoldersCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.IncludesSubFoldersCheckbox.AutoSize = true;
             this.IncludesSubFoldersCheckbox.Checked = true;
             this.IncludesSubFoldersCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.IncludesSubFoldersCheckbox.Location = new System.Drawing.Point(1, 324);
+            this.IncludesSubFoldersCheckbox.Enabled = false;
+            this.IncludesSubFoldersCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IncludesSubFoldersCheckbox.Location = new System.Drawing.Point(1, 313);
             this.IncludesSubFoldersCheckbox.Name = "IncludesSubFoldersCheckbox";
-            this.IncludesSubFoldersCheckbox.Size = new System.Drawing.Size(121, 17);
+            this.IncludesSubFoldersCheckbox.Size = new System.Drawing.Size(191, 24);
             this.IncludesSubFoldersCheckbox.TabIndex = 2;
             this.IncludesSubFoldersCheckbox.Text = "Include sub folders?";
             this.IncludesSubFoldersCheckbox.UseVisualStyleBackColor = true;
             // 
             // TotalItemsPicker
             // 
-            this.TotalItemsPicker.Location = new System.Drawing.Point(505, 320);
+            this.TotalItemsPicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.TotalItemsPicker.Enabled = false;
+            this.TotalItemsPicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalItemsPicker.Location = new System.Drawing.Point(695, 311);
             this.TotalItemsPicker.Maximum = new decimal(new int[] {
             10,
             0,
@@ -86,7 +107,7 @@
             0,
             0});
             this.TotalItemsPicker.Name = "TotalItemsPicker";
-            this.TotalItemsPicker.Size = new System.Drawing.Size(39, 20);
+            this.TotalItemsPicker.Size = new System.Drawing.Size(39, 26);
             this.TotalItemsPicker.TabIndex = 3;
             this.TotalItemsPicker.Value = new decimal(new int[] {
             3,
@@ -96,53 +117,102 @@
             // 
             // TotalItemsLabel
             // 
+            this.TotalItemsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.TotalItemsLabel.AutoSize = true;
-            this.TotalItemsLabel.Location = new System.Drawing.Point(419, 322);
+            this.TotalItemsLabel.Enabled = false;
+            this.TotalItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalItemsLabel.Location = new System.Drawing.Point(550, 313);
             this.TotalItemsLabel.Name = "TotalItemsLabel";
-            this.TotalItemsLabel.Size = new System.Drawing.Size(58, 13);
+            this.TotalItemsLabel.Size = new System.Drawing.Size(97, 20);
             this.TotalItemsLabel.TabIndex = 4;
             this.TotalItemsLabel.Text = "Total items";
             // 
             // GetRandomItemsButton
             // 
-            this.GetRandomItemsButton.Location = new System.Drawing.Point(1, 365);
+            this.GetRandomItemsButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GetRandomItemsButton.BackColor = System.Drawing.Color.Black;
+            this.GetRandomItemsButton.Enabled = false;
+            this.GetRandomItemsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetRandomItemsButton.ForeColor = System.Drawing.Color.White;
+            this.GetRandomItemsButton.Location = new System.Drawing.Point(1, 346);
             this.GetRandomItemsButton.Name = "GetRandomItemsButton";
-            this.GetRandomItemsButton.Size = new System.Drawing.Size(543, 38);
+            this.GetRandomItemsButton.Size = new System.Drawing.Size(733, 45);
             this.GetRandomItemsButton.TabIndex = 5;
             this.GetRandomItemsButton.Text = "Get random items";
-            this.GetRandomItemsButton.UseVisualStyleBackColor = true;
+            this.GetRandomItemsButton.UseVisualStyleBackColor = false;
             this.GetRandomItemsButton.Click += new System.EventHandler(this.GetRandomItemsButton_Click);
             // 
             // ShortlistOfUrlsGrid
             // 
             this.ShortlistOfUrlsGrid.AllowUserToAddRows = false;
             this.ShortlistOfUrlsGrid.AllowUserToDeleteRows = false;
+            this.ShortlistOfUrlsGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShortlistOfUrlsGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ShortlistOfUrlsGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ShortlistOfUrlsGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.ShortlistOfUrlsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ShortlistOfUrlsGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.SelectedColumn,
             this.NameColumn,
             this.UrlColumn});
-            this.ShortlistOfUrlsGrid.Location = new System.Drawing.Point(1, 421);
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ShortlistOfUrlsGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ShortlistOfUrlsGrid.Enabled = false;
+            this.ShortlistOfUrlsGrid.EnableHeadersVisualStyles = false;
+            this.ShortlistOfUrlsGrid.Location = new System.Drawing.Point(1, 397);
             this.ShortlistOfUrlsGrid.Name = "ShortlistOfUrlsGrid";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ShortlistOfUrlsGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.ShortlistOfUrlsGrid.RowHeadersVisible = false;
-            this.ShortlistOfUrlsGrid.Size = new System.Drawing.Size(543, 150);
+            this.ShortlistOfUrlsGrid.Size = new System.Drawing.Size(733, 180);
             this.ShortlistOfUrlsGrid.TabIndex = 6;
             // 
             // DisplayUrlsButton
             // 
-            this.DisplayUrlsButton.Location = new System.Drawing.Point(1, 578);
+            this.DisplayUrlsButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DisplayUrlsButton.BackColor = System.Drawing.Color.Black;
+            this.DisplayUrlsButton.Enabled = false;
+            this.DisplayUrlsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayUrlsButton.ForeColor = System.Drawing.Color.White;
+            this.DisplayUrlsButton.Location = new System.Drawing.Point(1, 580);
             this.DisplayUrlsButton.Name = "DisplayUrlsButton";
-            this.DisplayUrlsButton.Size = new System.Drawing.Size(543, 51);
+            this.DisplayUrlsButton.Size = new System.Drawing.Size(733, 45);
             this.DisplayUrlsButton.TabIndex = 7;
             this.DisplayUrlsButton.Text = "Display web pages";
-            this.DisplayUrlsButton.UseVisualStyleBackColor = true;
+            this.DisplayUrlsButton.UseVisualStyleBackColor = false;
             this.DisplayUrlsButton.Click += new System.EventHandler(this.DisplayUrlsButton_Click);
             // 
             // SelectedColumn
             // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.NullValue = false;
+            this.SelectedColumn.DefaultCellStyle = dataGridViewCellStyle2;
             this.SelectedColumn.HeaderText = "Select";
             this.SelectedColumn.Name = "SelectedColumn";
-            this.SelectedColumn.Width = 50;
+            this.SelectedColumn.Width = 80;
             // 
             // NameColumn
             // 
@@ -150,7 +220,7 @@
             this.NameColumn.HeaderText = "Name";
             this.NameColumn.Name = "NameColumn";
             this.NameColumn.ReadOnly = true;
-            this.NameColumn.Width = 200;
+            this.NameColumn.Width = 300;
             // 
             // UrlColumn
             // 
@@ -158,13 +228,14 @@
             this.UrlColumn.HeaderText = "URL";
             this.UrlColumn.Name = "UrlColumn";
             this.UrlColumn.ReadOnly = true;
-            this.UrlColumn.Width = 200;
+            this.UrlColumn.Width = 340;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 641);
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(738, 628);
             this.Controls.Add(this.DisplayUrlsButton);
             this.Controls.Add(this.ShortlistOfUrlsGrid);
             this.Controls.Add(this.GetRandomItemsButton);
@@ -174,6 +245,7 @@
             this.Controls.Add(this.FoldersTreeView);
             this.Controls.Add(this.RefreshFoldersButton);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Random Bookmark Reader";
             ((System.ComponentModel.ISupportInitialize)(this.TotalItemsPicker)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ShortlistOfUrlsGrid)).EndInit();
--- RandomBookmarkReader/MainForm.cs
+++ RandomBookmarkReader/MainForm.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Marcware.RandomBookmarkReader.BookmarkContainerClasses;
 using Marcware.RandomBookmarkReader.BookmarkManager;
@@ -50,17 +47,22 @@ namespace Marcware.RandomBookmarkReader
             public const string Url = "UrlColumn";
         }
 
+        private enum FormStatusTypes { Initialising, SelectingFolderOptions, SelectingFromShortlist }
+
         private BookmarkManagerClass BookmarkManager { get; set; }
 
         public MainForm()
         {
             InitializeComponent();
+            SetFormStatus(FormStatusTypes.Initialising);
         }
 
         private void RefreshFoldersButton_Click(object sender, EventArgs e)
         {
             BookmarkManager = new BookmarkManagerClass();
             ProcessResult<Dictionary<string, BrowserBookmarks>> readerResult = BookmarkManager.GetBookmarksFromAllBrowsers();
+            FoldersTreeView.Nodes.Clear();
+            ShortlistOfUrlsGrid.DataSource = null;
             if (readerResult.Success)
             {
                 foreach (BrowserBookmarks browser in readerResult.Result.Values)
@@ -68,6 +70,7 @@ namespace Marcware.RandomBookmarkReader
                     TreeNode browserNode = FoldersTreeView.Nodes.Add(browser.RootFolder.FolderId, browser.BrowserName);
                     AddChildFoldersToBookmarksTreeView(browser, browser.RootFolder, browserNode);
                 }
+                SetFormStatus(FormStatusTypes.SelectingFolderOptions);
             }
             else
             {
@@ -100,6 +103,7 @@ namespace Marcware.RandomBookmarkReader
                         {
                             row.Cells[ShortlistUrlGridColumns.Selected].Value = true;
                         }
+                        SetFormStatus(FormStatusTypes.SelectingFromShortlist);
                     }
                     else
                     {
@@ -127,6 +131,77 @@ namespace Marcware.RandomBookmarkReader
                     string url = row.Cells[ShortlistUrlGridColumns.Url].Value.ToString();
                     Process.Start(url);
                 }
+            }
+        }
+
+        private void SetFormStatus(FormStatusTypes formStatus)
+        {
+            switch (formStatus)
+            {
+                case FormStatusTypes.Initialising:                    
+                    IncludesSubFoldersCheckbox.Enabled = false;
+                    TotalItemsPicker.Enabled = false;                                        
+                    SetEnabled(false, FoldersTreeView);
+                    SetEnabled(false, ShortlistOfUrlsGrid);
+                    SetEnabled(false, GetRandomItemsButton, DisplayUrlsButton);
+                    break;
+                case FormStatusTypes.SelectingFolderOptions:
+                    IncludesSubFoldersCheckbox.Enabled = true;
+                    TotalItemsPicker.Enabled = true;
+                    SetEnabled(true, FoldersTreeView);
+                    SetEnabled(true, GetRandomItemsButton);
+                    SetEnabled(false, ShortlistOfUrlsGrid);
+                    SetEnabled(false, DisplayUrlsButton);
+                    break;
+                case FormStatusTypes.SelectingFromShortlist:                    
+                    IncludesSubFoldersCheckbox.Enabled = true;
+                    TotalItemsPicker.Enabled = true;
+                    SetEnabled(true, FoldersTreeView);
+                    SetEnabled(true, ShortlistOfUrlsGrid);
+                    SetEnabled(true, GetRandomItemsButton, DisplayUrlsButton);
+                    break;
+            }
+        }
+
+        private void SetEnabled(bool enabled, params Button[] buttons)
+        {
+            foreach (Button button in buttons)
+            {
+                if (enabled)
+                    button.BackColor = Color.Black;
+                else
+                    button.BackColor = Color.Silver;
+                button.Enabled = enabled;
+            }
+        }
+
+        private void SetEnabled(bool enabled, params TreeView[] treeViews)
+        {
+            foreach (TreeView treeView in treeViews)
+            {
+                if (enabled)
+                    treeView.BackColor = Color.FromArgb(255, 255, 128);
+                else
+                    treeView.BackColor = Color.FromArgb(255, 255, 192);
+                treeView.Enabled = enabled;
+            }
+        }
+
+        private void SetEnabled(bool enabled, params DataGridView[] gridViews)
+        {
+            foreach (DataGridView gridView in gridViews)
+            {
+                if (enabled)
+                {
+                    gridView.DefaultCellStyle.BackColor = Color.FromArgb(255, 255, 128);
+                    gridView.BackgroundColor = Color.FromArgb(255, 255, 128);
+                }
+                else
+                {
+                    gridView.DefaultCellStyle.BackColor = Color.FromArgb(255, 255, 192);
+                    gridView.BackgroundColor = Color.FromArgb(255, 255, 192);
+                }
+                gridView.Enabled = enabled;
             }
         }
     }
